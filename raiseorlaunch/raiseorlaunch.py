e0992017f72a6146cfa0f09c9130ee2584cc2263
Fabio <open-dynaMIX@users.noreply.github.com>
2017-11-21 09:03:37 +0100

    bump version

--- raiseorlaunch/raiseorlaunch.py
+++ raiseorlaunch/raiseorlaunch.py
@@ -11,7 +11,7 @@ from __future__ import print_function
 
 __title__ = 'raiseorlaunch'
 __description__ = 'A run-or-raise-application-launcher for i3 window manager.'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __license__ = 'MIT'
 __author__ = 'Fabio Rämi'
 
