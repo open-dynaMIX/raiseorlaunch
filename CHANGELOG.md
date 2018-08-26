31a6a237afac2e948d2715df536adbc20c0b764f
Fabio <open-dynaMIX@users.noreply.github.com>
2018-08-26 14:48:41 +0200

    added CHANGELOG.md; bump version

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# v2.1.0
+
+# Features
+ - Added flag to set initial workspace #15
+ - Move created windows to expected workspace #16
+ - Window cycling #17
+ - Use the new `timeout` argument for i3ipc main loop #19
--- raiseorlaunch/raiseorlaunch.py
+++ raiseorlaunch/raiseorlaunch.py
@@ -8,7 +8,7 @@ for i3 window manager.
 
 __title__ = 'raiseorlaunch'
 __description__ = 'A run-or-raise-application-launcher for i3 window manager.'
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 __license__ = 'MIT'
 __author__ = 'Fabio Rämi'
 
