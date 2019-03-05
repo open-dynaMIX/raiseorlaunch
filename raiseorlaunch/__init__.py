b6561466dbee2b2e09b3f05d0bd2bebe128da874
Fabio <open-dynaMIX@users.noreply.github.com>
2019-03-05 14:18:30 +0100

    Introduce black
    Also use isort and flake8 alongside of black in a pre-commit hook.

--- raiseorlaunch/__init__.py
+++ raiseorlaunch/__init__.py
@@ -1,8 +1,10 @@
-from raiseorlaunch.raiseorlaunch import (Raiseorlaunch,  # noqa
-                                         RaiseorlaunchError,  # noqa
-                                         check_positive,  # noqa
-                                         __title__,  # noqa
-                                         __description__,  # noqa
-                                         __version__,  # noqa
-                                         __license__,  # noqa
-                                         __author__)  # noqa
+from raiseorlaunch.raiseorlaunch import (  # noqa: F401
+    Raiseorlaunch,
+    RaiseorlaunchError,
+    check_positive,
+    __title__,
+    __description__,
+    __version__,
+    __license__,
+    __author__,
+)
--- raiseorlaunch/raiseorlaunch.py
+++ raiseorlaunch/raiseorlaunch.py
@@ -6,16 +6,17 @@ for i3 window manager.
 """
 
 
-__title__ = 'raiseorlaunch'
-__description__ = 'A run-or-raise-application-launcher for i3 window manager.'
-__version__ = '2.2.1'
-__license__ = 'MIT'
-__author__ = 'Fabio Rämi'
+__title__ = "raiseorlaunch"
+__description__ = "A run-or-raise-application-launcher for i3 window manager."
+__version__ = "2.2.1"
+__license__ = "MIT"
+__author__ = "Fabio Rämi"
 
 
-import sys
-import re
 import logging
+import re
+import sys
+
 try:
     import i3ipc
 except ImportError:
@@ -64,19 +65,22 @@ class Raiseorlaunch(object):
         leave_fullscreen (bool, optional): Leave fullscreen on target
                                            workspace.
     """
-    def __init__(self,
-                 command,
-                 wm_class=None,
-                 wm_instance=None,
-                 wm_title=None,
-                 workspace=None,
-                 target_workspace=None,
-                 scratch=False,
-                 con_mark=None,
-                 ignore_case=False,
-                 event_time_limit=2,
-                 cycle=False,
-                 leave_fullscreen=False):
+
+    def __init__(
+        self,
+        command,
+        wm_class=None,
+        wm_instance=None,
+        wm_title=None,
+        workspace=None,
+        target_workspace=None,
+        scratch=False,
+        con_mark=None,
+        ignore_case=False,
+        event_time_limit=2,
+        cycle=False,
+        leave_fullscreen=False,
+    ):
         self.command = command
         self.wm_class = wm_class
         self.wm_instance = wm_instance
@@ -107,18 +111,22 @@ class Raiseorlaunch(object):
          - ...workspace and init_workspace are not set to something different
         """
         if not self.wm_class and not self.wm_instance and not self.wm_title:
-            raise RaiseorlaunchError('You need to specify '
-                                     '"wm_class", "wm_instance" or "wm_title.')
+            raise RaiseorlaunchError(
+                "You need to specify " '"wm_class", "wm_instance" or "wm_title.'
+            )
         if (self.workspace or self.target_workspace) and self.scratch:
-            raise RaiseorlaunchError('You cannot use the scratchpad on a '
-                                     'specific workspace.')
+            raise RaiseorlaunchError(
+                "You cannot use the scratchpad on a " "specific workspace."
+            )
         if not check_positive(self.event_time_limit):
-            raise RaiseorlaunchError('The event time limit must be a positive '
-                                     'integer or float!')
+            raise RaiseorlaunchError(
+                "The event time limit must be a positive " "integer or float!"
+            )
         if self.workspace and self.target_workspace:
             if not self.workspace == self.target_workspace:
-                raise RaiseorlaunchError('Setting workspace and initial '
-                                         'workspace is ambiguous!')
+                raise RaiseorlaunchError(
+                    "Setting workspace and initial " "workspace is ambiguous!"
+                )
 
     @staticmethod
     def _log_format_con(window):
@@ -129,8 +137,8 @@ class Raiseorlaunch(object):
             str: '<Con: class="{}" instance="{}" title="{}" id={}>'
         """
         return '<Con: class="{}" instance="{}" title="{}" id={}>'.format(
-            window.window_class, window.window_instance, window.name,
-            window.id)
+            window.window_class, window.window_instance, window.name, window.id
+        )
 
     def _match_regex(self, regex, string_to_match):
         """
@@ -156,13 +164,15 @@ class Raiseorlaunch(object):
         Returns:
             bool: True for match, False otherwise.
         """
-        for (pattern, value) in [(self.wm_class, window.window_class),
-                                 (self.wm_instance, window.window_instance),
-                                 (self.wm_title, window.name)]:
+        for (pattern, value) in [
+            (self.wm_class, window.window_class),
+            (self.wm_instance, window.window_instance),
+            (self.wm_title, window.name),
+        ]:
             if pattern and not self._match_regex(pattern, value):
                 return False
 
-        logger.debug('Window match: {}'.format(self._log_format_con(window)))
+        logger.debug("Window match: {}".format(self._log_format_con(window)))
         return True
 
     def _get_window_list(self):
@@ -173,16 +183,20 @@ class Raiseorlaunch(object):
             list: Instances of Con()
         """
         if not self.workspace:
-            logger.debug('Getting list of windows.')
+            logger.debug("Getting list of windows.")
             leaves = self.tree.leaves()
             if self.scratch:
-                return [l for l in leaves if
-                        l.parent.scratchpad_state in ['changed', 'fresh']]
+                return [
+                    l
+                    for l in leaves
+                    if l.parent.scratchpad_state in ["changed", "fresh"]
+                ]
             else:
                 return leaves
         else:
-            logger.debug('Getting list of windows on workspace: {}.'
-                         .format(self.workspace))
+            logger.debug(
+                "Getting list of windows on workspace: {}.".format(self.workspace)
+            )
             workspaces = self.tree.workspaces()
             for workspace in workspaces:
                 if workspace.name == self.workspace:
@@ -220,7 +234,7 @@ class Raiseorlaunch(object):
                     found.append(leave)
 
             if len(found) > 1:
-                logger.debug('Multiple windows match the properties.')
+                logger.debug("Multiple windows match the properties.")
 
         return found if found else None
 
@@ -228,8 +242,8 @@ class Raiseorlaunch(object):
         """
         Run the specified command with exec.
         """
-        command = 'exec {}'.format(self.command)
-        logger.debug('Executing command: {}'.format(command))
+        command = "exec {}".format(self.command)
+        logger.debug("Executing command: {}".format(command))
         self.i3.command(command)
 
     def set_con_mark(self, window):
@@ -239,10 +253,12 @@ class Raiseorlaunch(object):
         Args:
             window: Instance of Con()
         """
-        logger.debug('Setting con_mark "{}" on window: {}'
-                     .format(self.con_mark,
-                             self._log_format_con(window)))
-        window.command('mark {}'.format(self.con_mark))
+        logger.debug(
+            'Setting con_mark "{}" on window: {}'.format(
+                self.con_mark, self._log_format_con(window)
+            )
+        )
+        window.command("mark {}".format(self.con_mark))
 
     def focus_window(self, window):
         """
@@ -251,9 +267,8 @@ class Raiseorlaunch(object):
         Args:
             window: Instance of Con()
         """
-        logger.debug('Focusing window: {}'.format(
-            self._log_format_con(window)))
-        window.command('focus')
+        logger.debug("Focusing window: {}".format(self._log_format_con(window)))
+        window.command("focus")
 
     def get_current_workspace(self):
         """
@@ -271,14 +286,20 @@ class Raiseorlaunch(object):
         Args:
             window: Instance of Con().
         """
-        logger.debug('Enabling floating mode on newly created window: {}'
-                     .format(self._log_format_con(window)))
+        logger.debug(
+            "Enabling floating mode on newly created window: {}".format(
+                self._log_format_con(window)
+            )
+        )
         # Somehow this is needed to retain window geometry
         # (e.g. when using xterm -geometry)
-        window.command('floating enable')
-        logger.debug('Moving newly created window to the scratchpad: {}'
-                     .format(self._log_format_con(window)))
-        window.command('move scratchpad')
+        window.command("floating enable")
+        logger.debug(
+            "Moving newly created window to the scratchpad: {}".format(
+                self._log_format_con(window)
+            )
+        )
+        window.command("move scratchpad")
 
     def show_scratch(self, window):
         """
@@ -287,9 +308,12 @@ class Raiseorlaunch(object):
         Args:
             window: Instance of Con().
         """
-        logger.debug('Toggling visibility of scratch window: {}'.format(
-            self._log_format_con(window)))
-        window.command('scratchpad show')
+        logger.debug(
+            "Toggling visibility of scratch window: {}".format(
+                self._log_format_con(window)
+            )
+        )
+        window.command("scratchpad show")
 
     def switch_to_workspace_by_name(self, name):
         """
@@ -298,8 +322,8 @@ class Raiseorlaunch(object):
         Args:
             name (str): workspace name
         """
-        logger.debug('Switching to workspace: {}'.format(name))
-        self.i3.command('workspace {}'.format(name))
+        logger.debug("Switching to workspace: {}".format(name))
+        self.i3.command("workspace {}".format(name))
 
     @staticmethod
     def move_con_to_workspace_by_name(window, workspace):
@@ -310,8 +334,8 @@ class Raiseorlaunch(object):
             window: Instance of Con().
             workspace: str
         """
-        logger.debug('Moving window to workspace: {}'.format(workspace))
-        window.command('move container to workspace {}'.format(workspace))
+        logger.debug("Moving window to workspace: {}".format(workspace))
+        window.command("move container to workspace {}".format(workspace))
 
     def leave_fullscreen_on_workspace(self, workspace_name, exceptions=None):
         """
@@ -325,19 +349,15 @@ class Raiseorlaunch(object):
             None
         """
         exceptions = exceptions if exceptions else []
-        cons_on_ws = self.tree.find_named(
-            '^{}$'.format(workspace_name)
-        )
+        cons_on_ws = self.tree.find_named("^{}$".format(workspace_name))
         cons = cons_on_ws[0].find_fullscreen() if cons_on_ws else []
         for con in cons:
-            if con.type == 'workspace' or con in exceptions:
+            if con.type == "workspace" or con in exceptions:
                 continue
             logger.debug(
-                'Leaving fullscreen for con: {}'.format(
-                    self._log_format_con(con)
-                )
+                "Leaving fullscreen for con: {}".format(self._log_format_con(con))
             )
-            con.command('fullscreen')
+            con.command("fullscreen")
 
     def _choose_if_multiple(self, running):
         """
@@ -357,16 +377,20 @@ class Raiseorlaunch(object):
 
         window = running[0]
         if self.target_workspace:
-            multi_msg = ('Found multiple windows that match the '
-                         'properties. Using the first in the tree, '
-                         'preferably on target workspace.')
+            multi_msg = (
+                "Found multiple windows that match the "
+                "properties. Using the first in the tree, "
+                "preferably on target workspace."
+            )
             for w in running:
                 if w.workspace().name == self.target_workspace:
                     window = w
                     break
         else:
-            multi_msg = ('Found multiple windows that match the '
-                         'properties. Using the first in the tree.')
+            multi_msg = (
+                "Found multiple windows that match the "
+                "properties. Using the first in the tree."
+            )
 
         logger.debug(multi_msg)
         return window
@@ -389,9 +413,11 @@ class Raiseorlaunch(object):
 
         window = self._choose_if_multiple(running)
 
-        logger.debug('Application is running on workspace "{}": {}'
-                     .format(window.workspace().name,
-                             self._log_format_con(window)))
+        logger.debug(
+            'Application is running on workspace "{}": {}'.format(
+                window.workspace().name, self._log_format_con(window)
+            )
+        )
         if self.scratch:
             self._handle_running_scratch(window)
         else:
@@ -408,10 +434,12 @@ class Raiseorlaunch(object):
             self.focus_window(window)
         else:
             if self.current_ws.name == self.get_current_workspace().name:
-                logger.debug('We\'re on the right workspace. '
-                             'Switching anyway to retain '
-                             'workspace_back_and_forth '
-                             'functionality.')
+                logger.debug(
+                    "We're on the right workspace. "
+                    "Switching anyway to retain "
+                    "workspace_back_and_forth "
+                    "functionality."
+                )
                 self.switch_to_workspace_by_name(self.current_ws.name)
 
     def _handle_running_scratch(self, window):
@@ -436,7 +464,7 @@ class Raiseorlaunch(object):
         Args:
             windows: List with instances of Con().
         """
-        logger.debug('Cycling through matching windows.')
+        logger.debug("Cycling through matching windows.")
         switch = False
         w = None
         windows.append(windows[0])
@@ -448,14 +476,15 @@ class Raiseorlaunch(object):
                 switch = True
 
         if w:
-            logger.debug('Application is running on workspace "{}": {}'
-                         .format(w.workspace().name,
-                                 self._log_format_con(w)))
+            logger.debug(
+                'Application is running on workspace "{}": {}'.format(
+                    w.workspace().name, self._log_format_con(w)
+                )
+            )
 
             self.focus_window(w)
         else:
-            logger.error('No running windows received. '
-                         'This should not happen!')
+            logger.error("No running windows received. " "This should not happen!")
 
     def _handle_not_running(self):
         """
@@ -467,7 +496,8 @@ class Raiseorlaunch(object):
 
         if self.leave_fullscreen:
             self.leave_fullscreen_on_workspace(
-                self.target_workspace or self.current_ws.name)
+                self.target_workspace or self.current_ws.name
+            )
 
         self.i3.on("window::new", self._callback_new_window)
         self.run_command()
@@ -481,8 +511,7 @@ class Raiseorlaunch(object):
         the scratchpad and setting con_marks.
         """
         window = event.container
-        logger.debug('Event callback: {}'
-                     .format(self._log_format_con(window)))
+        logger.debug("Event callback: {}".format(self._log_format_con(window)))
 
         if self._compare_running(window):
             if self.scratch:
@@ -508,5 +537,5 @@ class Raiseorlaunch(object):
         if running:
             self._handle_running(running)
         else:
-            logger.debug('Application is not running.')
+            logger.debug("Application is not running.")
             self._handle_not_running()
