2630ffb422a3c7f83ad0b7899f3a8cb5828a1768
Fabio Ambauen <1833932+open-dynaMIX@users.noreply.github.com>
2022-02-07 21:40:35 +0100

    chore: prepare release 2.3.5

--- raiseorlaunch/raiseorlaunch.py
+++ raiseorlaunch/raiseorlaunch.py
@@ -8,7 +8,7 @@ for i3 window manager.
 
 __title__ = "raiseorlaunch"
 __description__ = "A run-or-raise-application-launcher for i3 window manager."
-__version__ = "2.3.4"
+__version__ = "2.3.5"
 __license__ = "MIT"
 __author__ = "Fabio Ambauen"
 
